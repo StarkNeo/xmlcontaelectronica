f1ae10aacdb21574965afec2ed811e641c887501
Jesus Hernandez <jesushdzusa@gmail.com>
2025-11-26 17:17:11 -0600

    removing crypto calls

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import webview
 import os
 from app import app
 
-def run_flask():
-    app.run(debug=False)
+#def run_flask():
+ #   app.run(debug=False)
+
     
 if __name__ == '__main__':
-    threading.Thread(target=run_flask).start()
-    webview.create_window("XML Creator", "http://127.0.0.1:5000")
+    app.run(debug=True)
+    #threading.Thread(target=run_flask).start()
+    #webview.create_window("XML Creator", "http://127.0.0.1:5000")
